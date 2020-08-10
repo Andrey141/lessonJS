16f588a3cee4fdf867d288721701781d6686a41e
Andrey Semchenko <ak445@mail.ru>
2020-08-10 14:34:42 +0300

    lesson02

--- script/main.js
+++ script/main.js
@@ -1,11 +1,19 @@
 
-let money,
-    income,
-    addExpenses,
-    deposit,
-    mission,
-    period;
+let money = 235,
+    income = 'Фриланс',
+    addExpenses = 'Интернет, Такси, Коммуналка',
+    deposit = true,
+    mission = 936,
+    period = 3,
+    budgetDay = 218;
 
-alert('Hello world!');
+console.log(typeof money);
+console.log(typeof income);
+console.log(typeof deposit);
 
-console.log('Hello Glo_Academy!');
+console.log(addExpenses.length);
+console.log('"Период равен ' + period + ' месяца." ' + 
+'Цель заработать ' + mission + ' долларов.');
+
+console.log(addExpenses.toLowerCase().split(', '));
+console.log(budgetDay);
