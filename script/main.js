91b0d391efa52a951e01be451205a8ecc8d5313b
Andrey Semchenko <ak445@mail.ru>
2020-08-14 00:48:08 +0300

    lesson05. Заменил название функции f1 --> getBudgetDay

--- script/main.js
+++ script/main.js
@@ -1,3 +1,8 @@
+let isNumber = function(n) {
+return !isNaN(parseFloat(n)) && isFinite(n);
+};
+
+
 
  'use strict';
 let money = 235,
@@ -7,6 +12,15 @@ let money = 235,
     period = 3,
     deposit = true;
    
+let start = function (){
+    do
+        money = prompt('Укажите ваш месячный доход.');
+    while (!isNumber(money)){
+        //money = prompt('Укажите ваш месячный доход.');
+    }
+};
+
+start();
 
 function showTypeOf (data){
     console.log(typeof data);
@@ -16,25 +30,20 @@ showTypeOf(income);
 showTypeOf(deposit);
 //-------------------------------------------------------------------------
 
-money = Number(prompt('Укажите ваш месячный доход.'));
+//money = Number(prompt('Укажите ваш месячный доход.'));
 
 addExpenses = prompt('Перечислите возможные расходы за расчитываемый период через запятую.');
+addExpenses = addExpenses.toLowerCase().split(', ');
+console.log(addExpenses);
 
 deposit = confirm('Есть ли у вас депозит "ОК - ДА", "ОТМЕНА - НЕТ"');
 
-let amount1 = Number(prompt('Во сколько это обойдется')),
-    amount2 = Number(prompt('Во сколько это обойдется'));
+//let expenses1 = prompt('Введиет обязательную статью расходов');
+//    amount1 = Number(prompt('Во сколько это обойдется'));
+//    expenses2 = prompt('Введиет обязательную статью расходов');
+//    amount2 = Number(prompt('Во сколько это обойдется'));
 
 function getStatusIncome(budgetDay){
-/*if (budgetDay >= 1200) 
-    console.log('У вас высокий уровень дохода.');
-else if (budgetDay >=600 && budgetDay < 1200 )
-        console.log('У вас средний уровень дохода.');
-     else if (budgetDay < 600 && budgetDay > 0 ) 
-            console.log('У вас низкий уровень дохода');
-           else {
-               console.liog('У вас что то пошло не так.')
-           };*/
     budgetDay >= 1200? console.log('У вас высокий уровень дохода.') : 
     budgetDay >=600 && budgetDay < 1200? console.log('У вас средний уровень дохода.'):
     budgetDay < 600 && budgetDay > 0? console.log('У вас низкий уровень дохода') : console.log('У вас что то пошло не так.');
@@ -42,20 +51,36 @@ else if (budgetDay >=600 && budgetDay < 1200 )
 }
 //-------------------------------------------------------------------------------
 //Функция возвращает сумму всех обязательных расходов за месяц 
-let  amount;
-function getExpensesMonth(a,b) {
-    return a+b;
-}
-console.log('Ваши расходы в месяц состовляют: ', amount = getExpensesMonth(amount1, amount2));
+////let  amount;*/
+let expenses = [];
 
-addExpenses = addExpenses.toLowerCase().split(', ');
-console.log(addExpenses);
+let  getExpensesMonth = function() {
+    let sum = 0;
+
+    for(let i = 0; i < 2; i++) {
+       
+        expenses[i] = prompt('Введиет обязательную статью расходов');
+        sum = prompt('Во сколько это обойдется');
+        while ((isNaN(parseFloat(sum)) || (sum ==='') || (sum === null))) {
+            sum = prompt('Во сколько это обойдется');
+        }
+    }
+    console.log(expenses);
+    return sum;
+};
+
+
+let expensesAmount = getExpensesMonth();
+
+console.log('Ваши расходы в месяц состовляют: ', expensesAmount);
+
+
 
 //Функция возвращает накопления за месяц (доходы минус расходы)
 const getAccumulatedMonth = function(a, b){
     return a - b;
 };
-let accumulatedMonth = getAccumulatedMonth(money, amount);
+let accumulatedMonth = getAccumulatedMonth(money, expensesAmount);
 
 //Функция для подсчета за какой период будет достигнута цель
 const getTargetMonth = function (a, b){
@@ -65,10 +90,10 @@ let target = getTargetMonth(mission, accumulatedMonth);
 console.log('Вы достигните своей цели через: ', target, 'месяцев');
 
 //Функция для подсчета дневного бюджета
-const f1 = function (a){
+const getBudgetDay = function (a){
     return a / 30;
 };
-let budgetDay = f1(accumulatedMonth);
+let budgetDay = getBudgetDay(accumulatedMonth);
 console.log('Ваш доход в день составляет: ', budgetDay);
 
 getStatusIncome(budgetDay);
