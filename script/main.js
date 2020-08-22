e7180ed909284d24a6a000825304e9631801e762
Andrey Semchenko <ak445@mail.ru>
2020-08-22 14:04:15 +0300

    Исправил подсчет бюджет в месяц. Округлил до целого расчет бюджета в день

--- script/main.js
+++ script/main.js
@@ -8,9 +8,6 @@ let money,
     
 };
 start();
-////let isNumber = function(n) {
-/////return !isNaN(parseFloat(n)) && isFinite(n);
-////};
 
 let appData = {
     income: {},
@@ -42,16 +39,8 @@ let appData = {
         let sum = 0;
         for(let key in appData.expenses) {
             sum +=  +appData.expenses[key];
-            
-
-           //// expenses[i] = prompt('Введиет обязательную статью расходов');
-           //// sum = prompt('Во сколько это обойдется');
-           //// while ((isNaN(parseFloat(sum)) || (sum ==='') || (sum === null))) {
-           ////     sum = prompt('Во сколько это обойдется');
-           //// }
-           
         }
-        //console.log(expenses);
+
         return sum;
     },
     getBudget: function(){
@@ -59,14 +48,8 @@ let appData = {
         for (let key in appData.expenses){
         sum += +appData.expenses[key];
         }
-        console.log(sum);
-        appData.budgetMonth = appData.mission - sum;
-        appData.budgetDay = appData.budgetMonth / 30;
-
-        console.log('бюджет в месяц составляет', appData.budgetMonth);
-        console.log('бюджет в день составляет', appData.budgetDay);
-        
-        
+        appData.budgetMonth = appData.budget - sum;  // исправил расчет
+        appData.budgetDay = Math.floor(appData.budgetMonth / 30);  // округлил до целого
     },
     getTargetMonth: function (){
         return appData.mission / appData.budgetMonth;
@@ -80,71 +63,26 @@ let appData = {
 };
 appData.asking();
 
+//let expenses = [];
 
-
-////let 
-////    addExpenses = 'Интернет, Такси, Коммуналка',
-////    deposit = true;
-
-
-
-////function showTypeOf (data){
-////    console.log(typeof data);
-////}   
-////showTypeOf(money);
-////showTypeOf(appData.income);
-////showTypeOf(appData.deposit);
-//-------------------------------------------------------------------------
-
-////deposit = confirm('Есть ли у вас депозит "ОК - ДА", "ОТМЕНА - НЕТ"');
-
-////function getStatusIncome(budgetDay){
-////    budgetDay >= 1200? console.log('У вас высокий уровень дохода.') : 
-////    budgetDay >=600 && budgetDay < 1200? console.log('У вас средний уровень дохода.'):
-////    budgetDay < 600 && budgetDay > 0? console.log('У вас низкий уровень дохода') : console.log('У вас что то пошло не так.');
-////
-////}
-//-------------------------------------------------------------------------------
-
-let expenses = [];
-
-////let  getExpensesMonth = function() {
-////    let sum = 0;
-////
-////    for(let i = 0; i < 2; i++) {
-////       
-////        expenses[i] = prompt('Введиет обязательную статью расходов');
-////        sum = prompt('Во сколько это обойдется');
-////        while ((isNaN(parseFloat(sum)) || (sum ==='') || (sum === null))) {
-////            sum = prompt('Во сколько это обойдется');
-////        }
-////    }
-////    console.log(expenses);
-////    return sum;
-////};
 
 let expensesAmount = appData.getExpensesMonth();
 
 console.log('Ваши расходы в месяц состовляют: ', expensesAmount);
 
-//Функция возвращает накопления за месяц (доходы минус расходы)
-////const getAccumulatedMonth = function(a, b){
-////    return a - b;
-////};
 let accumulatedMonth = appData.getBudget(money, expensesAmount);
 
-//Функция для подсчета за какой период будет достигнута цель
-////const getTargetMonth = function (a, b){
-////    return appData.mission / accumulatedMonth;
-////};
 let target = Math.ceil(appData.getTargetMonth(appData.mission, accumulatedMonth));
-console.log('Цель не будет достигнута за: ', target );
-
-//Функция для подсчета дневного бюджета
-////const getBudgetDay = function (a){
-////    return a / 30;
-////};
-////let budgetDay = getBudgetDay(accumulatedMonth);
+console.log('Цель будет достигнута за: ', target );
 
 appData.getStatusIncome(appData.budgetDay);
 console.log(appData.expenses);
+
+function getAppData(){
+    console.log('Наша программа включает в себя данные:')
+    for (let key in appData){
+        console.log(key + '-' + appData[key]);
+    }
+};
+
+getAppData();
